c16f900034ce06f0ff5fb707b66259925e9bf6ee
RSmithCoding <111738784+RSmithCoding@users.noreply.github.com>
2022-09-30 21:21:31 +0100

    Started new program to test curses library

---
+++ DebtRelief.py
@@ -0,0 +1,3 @@
+# Small Application to test with curses library. Will allow you to add debts and bank balance and warn you about up coming debts to put money aside.
+
+# Main menu; Add Debts, show debts, update balance and show upcoming debts
