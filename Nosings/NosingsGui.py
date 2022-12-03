380b23d9810b6e1d4873d9d821b54150d5ca9253
RSmithCoding <rsmith8105@gmail.com>
2022-12-03 12:33:17 +0000

    Updates to Nosings

--- Nosings/NosingsGui.py
+++ Nosings/NosingsGui.py
@@ -8,27 +8,67 @@ root.title("Nosing Calculator")
 root.resizable(FALSE,FALSE)
 root.geometry("850x780")
 
-heading_frame = Frame(root, height=20, width=850, padx=0, pady=0)
-heading_frame.grid(row=0, columnspan=5)
+#root.grid_rowconfigure(1, weight=1)
+#root.grid_rowconfigure(2, weight=7)
+#root.grid_rowconfigure(3, weight=1)
+#root.grid_rowconfigure(4, weight=1)
 
-heading_label = Label(heading_frame,text="Nosing Calculator")
-heading_label.grid(row=0, column=0)
-heading_label.config(font=("Arial",24))
+"""HEADINGS"""
+heading_frame = Frame(root, bg="blue")
+heading_frame.grid(row=1, column=1, columnspan=5)
 
-sub_heading_label = Label(heading_frame, text="Sizes Entered...")
-sub_heading_label.grid(row=0, column=1)
-sub_heading_label.config(font=("Arial",24))
+#heading_label = Label(heading_frame,text="Order Summary")
+#heading_label.grid(row=1,column=0,columnspan=3)
+#heading_label.config(font=("Arial",24))
 
-order_summery_frame = Frame(root, height=500, width=700,bg='#93c47d')   #green
-order_summery_frame.grid(row=1, rowspan=3, column=0, columnspan=3)
+#sub_heading_label = Label(heading_frame, text="Sizes Entered...")
+#sub_heading_label.grid(row=0,column=3,columnspan=2)
+#sub_heading_label.config(font=("Arial",24))
 
-input_frame = Frame(root, height=40, width=700,bg='#E6337A')    #pink
-input_frame.grid(row=4, column=0, rowspan=1, columnspan=3)
+"""ORDER SUMMARY AREA"""
+order_summary_frame = Frame(root, bg='green')
+order_summary_frame.grid(row=2, column=1, columnspan=3)
 
-buttons_from = Frame(root, height=40, width=700,bg='#2986cc')   #blue
-buttons_from.grid(row=5, column=0, rowspan=1, columnspan=3)
+#summary_label = Label(order_summary_frame,text="0.234   ...    x2")
+#summary_label.grid(row=1,column=1)
+#summary_label.config(font=("Arial",24))
 
-size_log_frame = Frame(root, height=760, width=150,bg='#cc0000')    #red
-size_log_frame.grid(row=1, column=4 ,rowspan=3, columnspan=2)
+"""SIZE LOG AREA"""
+size_log_frame = Frame(root, bg='red')  
+size_log_frame.grid(row=1, column=4, columnspan=2)
+
+#size_log_label = Label(size_log_frame,text="0.242")
+#size_log_label.grid(row=1,column=1)
+#size_log_label.config(font=("Arial",24))
+
+"""INPUT AREA"""
+input_frame = Frame(root, bg='yellow')  
+input_frame.grid(row=3, column=1, columnspan=5)
+
+#size_entry = Entry(input_frame,width=20)
+#size_entry.grid(row=2,column=0)
+#size_entry.config(font=("Arial",24),justify="right")
+
+#running_total = Label(input_frame, text="0",padx=100)
+#running_total.grid(row=2,column=1)
+#running_total.config(font=("Arial",24),justify="right")
+
+# BUTTONS
+buttons_frame = Frame(root, bg='orange')  
+buttons_frame.grid(row=4, column=1, columnspan=5)
+
+#pdf_button = Button(buttons_frame,text="Create PDF",padx=10,pady=10)
+#pdf_button.grid(row=3, column=0)
+
+#summary_button = Button(buttons_frame,text="Create Summary",padx=10,pady=10)
+#summary_button.grid(row=3,column=1)
+
+#remove_button = Button(buttons_frame,text="Remove Last Size",padx=10,pady=10)
+#remove_button.grid(row=3,column=2)
+
+#close_button = Button(buttons_frame,text="Close",padx=25,pady=10)
+#close_button.grid(row=3,column=3)
+
+
 
 root.mainloop()
